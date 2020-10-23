76b101c4c7d675ca0cabfab2e94612fdaaeec7b6
kelseystiff <kelsey>
2020-10-23 18:00:15 -0500

    second commit

--- quiz.js
+++ quiz.js
@@ -1,24 +1,5 @@
-//api url
-url= `http://acnhapi.com/v1/villagers` //${randomVillagerNumber}
-
-//canvas image creation
-let canvas = document.querySelector('#canvas')
-let context = canvas.getContext('2d')
-let image = new Image()
-
-//buttons
-let answer_buttons = document.getElementById('answer_buttons')
-let result = document.getElementById('result')
-let next_button = document.getElementById('next')
-let buttons_array = []
-let correctAnswersButton = document.querySelector('#correct-answer')
-let wrongAnswerButtons = document.querySelectorAll('.wrong-answer')
-
-let count = 1;
-let score = 0;
-let scores = []
-
-let names = []
+//only one button click - disable all butttons
+//show right or wrong
 
 function random(data){
     let keys = Object.keys(data);
@@ -36,77 +17,107 @@ function shuffle(array) {
     return array;
 }
 
-function set_up(data){
-    image.addEventListener('load', function (){
-        context.drawImage(image,(canvas.width-image.width)/2, (canvas.height-image.height)/2)
-    })
+function generate_question(){
+    //api url
+    url= `http://acnhapi.com/v1/villagers` //${randomVillagerNumber}
 
-    let villager = random(data)
-    let correctVillagerName = villager.name["name-USen"]
-    image.src = villager["image_uri"]
-    console.log(correctVillagerName)
-    names.push(correctVillagerName)
+    //canvas image creation
+    let canvas = document.querySelector('#canvas')
+    let context = canvas.getContext('2d')
+    let image = new Image()
 
-    for(let i=0; i<3; i++){
-        names.push(random(data).name["name-USen"])
-    }
-    shuffle(names)
-    names.forEach(function (n){
-        console.log(n)
-        buttons = document.createElement('button')
-        buttons.innerHTML = n
-        buttons.id = n
-        buttons_array.push(buttons)
-    })
-}
+    //buttons
+    let answer_buttons = document.getElementById('answer_buttons')
+    let result = document.getElementById('result')
+    let buttons_array = []
 
+    let names = []
 
-//call api
-fetch(url)
-    .then( (response) => {
-        // response is all the things from the server
-        console.log(response) // logging response to check if working
-        // extract JSON
-        return response.json()
-    })
-    .then( function(data) { //after extracting object from API loop through it for info
-        let villager = random(data)
-        let correctVillagerName = villager.name["name-USen"]
-        set_up(data)
-        buttons_array.forEach(function (button){
-            if(button.innerHTML === correctVillagerName){
-                button.id = 'correct-answer'
-            }
-            else{
-                button.class = 'wrong-answer'
-            }
-            answer_buttons.appendChild(button)
-            button.addEventListener('click', function (){
-
+    fetch(url)
+        .then( (response) => {
+            // response is all the things from the server
+            console.log(response) // logging response to check if working
+            // extract JSON
+            return response.json()
+        })
+        .then( function(data) { //after extracting object from API loop through it for info
+            image.addEventListener('load', function (){
+                context.drawImage(image,(canvas.width-image.width)/2, (canvas.height-image.height)/2)
             })
 
-            next_button.addEventListener('click', function (){
-                count++
-                console.log(score)
-                if (count < 10){
-                    result.innerHTML = ''
-                    names = []
-                    buttons_array = []
-                    answer_buttons.innerHTML = ''
-                    set_up(data)
-                }
-                else {
-                    let container = document.getElementById('container')
-                    document.body.removeChild(container)
-                    result.innerHTML = `SCORE = ${score}`
-                }
+            let villager = random(data)
+            let correctVillagerName = villager.name["name-USen"]
+            image.src = villager["image_uri"]
+            console.log(correctVillagerName)
+            names.push(correctVillagerName)
+
+            for(let i=0; i<3; i++){
+                names.push(random(data).name["name-USen"])
+            }
+            shuffle(names)
+            names.forEach(function (n){
+                console.log(n)
+                let buttons = document.createElement('button')
+                buttons.innerHTML = n
+                buttons.name = 'answers'
+                buttons_array.push(buttons)
+                buttons_array.forEach(function (button){
+                    if(button.innerHTML === correctVillagerName){
+                        button.id = 'correct-answer'
+                    }
+                    else{
+                        button.class = 'wrong-answer'
+                    }
+                    answer_buttons.appendChild(button)
+
+                    button.addEventListener('click', function (){
+                        let nodes = answer_buttons.getElementsByTagName('button')
+                        for (let i = 0; i < nodes.length; i++) {
+                            nodes[i].disabled = true
+                        }
+                        if (button.id === 'correct-answer'){
+                            button.style.background = 'lime'
+                            result.innerHTML = 'CORRECT!'
+                            score++
+                        }
+                        else {
+                            button.style.background = 'red'
+                            result.innerHTML = `WRONG. The correct answer is ${correctVillagerName}`
+
+                        }
+                    })
+                })
             })
+        }) //if issues connecting to API error displays
+        .catch( error => {
+            console.log(error)
+            alert('ERROR')
         })
 
+}
+function game(){
+    let count = 1;
+    let score = 0;
+    let next_button = document.getElementById('next')
+    let score_placeHolder = document.getElementById('score')
 
+    generate_question()
 
-    }) //if issues connecting to API error displays
-    .catch( error => {
-        console.log(error)
-        alert('ERROR')
+    next_button.addEventListener('click', function (){
+        count++
+        if (count < 10){
+            result.innerHTML = ''
+            names = []
+            buttons_array = []
+            answer_buttons.innerHTML = ''
+            generate_question()
+        }
+        else {
+            let container = document.getElementById('container')
+            document.body.removeChild(container)
+            score_placeHolder.innerHTML = `SCORE = ${score}`
+        }
     })
+}
+game()
+
